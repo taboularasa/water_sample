6c7d0007fb766c017c0ecc934ff939cb2056414a
David Elliott <david@gophilosophie.com>
2015-06-24 23:46:53 -0700

    adds spec for WaterSample#factor

--- spec/models/water_sample_spec.rb
+++ spec/models/water_sample_spec.rb
@@ -1,5 +1,6 @@
 require 'pry-byebug'
 require_relative '../../app/models/water_sample'
+require_relative '../../db/db_adapter'
 
 RSpec.describe WaterSample do
   describe '::find' do
@@ -10,6 +11,42 @@ RSpec.describe WaterSample do
       expect(sample2.bromoform).to eq(0.00487)
       expect(sample2.bromodichloromethane).to eq(0.00547)
       expect(sample2.dibromichloromethane).to eq(0.0109)
+    end
+  end
+
+  describe '#factor' do
+    it 'multiplies Trihalomethanes with their factors and returns the sum' do
+      factor_weights_id = 2
+      sample2 = WaterSample.find(2)
+
+      factor_weights = DbAdapter.instance.client.query(
+        "SELECT * FROM factor_weights WHERE id = #{factor_weights_id}"
+      ).first
+
+      adjusted_chloroform =
+        sample2.chloroform *
+        factor_weights['chloroform_weight']
+
+      adjusted_bromoform =
+        sample2.bromoform *
+        factor_weights['bromoform_weight']
+
+      adjusted_bromodichloromethane =
+        sample2.bromodichloromethane *
+        factor_weights['bromodichloromethane_weight']
+
+      adjusted_dibromichloromethane =
+        sample2.dibromichloromethane *
+        factor_weights['dibromichloromethane_weight']
+
+      expected_result =
+        adjusted_chloroform +
+        adjusted_bromoform +
+        adjusted_bromodichloromethane +
+        adjusted_dibromichloromethane
+
+      result = sample2.factor(factor_weights_id)
+      expect(result).to eq(expected_result)
     end
   end
 end

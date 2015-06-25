becef3d1466b98a88b36db91b756f74326712847
David Elliott <david@gophilosophie.com>
2015-06-25 00:18:23 -0700

    adds WaterSample#to_hash

--- app/models/water_sample.rb
+++ app/models/water_sample.rb
@@ -3,8 +3,9 @@ require_relative '../../db/db_adapter'
 class WaterSample
   TRIHALOMETHANES =
     [:chloroform, :bromoform, :bromodichloromethane, :dibromichloromethane]
+  ATTRIBUTES = [:id, :site] + TRIHALOMETHANES
 
-  attr_accessor(:id, :site, *TRIHALOMETHANES)
+  attr_accessor(*ATTRIBUTES)
 
   def self.find(sample_id)
     results =
@@ -22,22 +23,45 @@ class WaterSample
   end
 
   def factor(factor_weights_id)
-    factor_weights = self.class.db.client.query(
-      "SELECT * FROM factor_weights WHERE id = #{factor_weights_id}"
-    ).first
+    factor_weights = find_factor_weights(factor_weights_id)
 
     trihalomethanes.inject(0) do |accumulator, element|
       accumulator += send(element) * factor_weights["#{element.to_s}_weight"]
     end
   end
 
-  def self.db
-    @db ||= DbAdapter.instance
+  def to_hash(include_factors = false)
+    attributes_hash = attributes.each_with_object({}) {|e, h| h[e] = send(e) }
+    return attributes_hash unless include_factors
+
+    factors_hash = all_factor_weights.each_with_object({}) do |element, hash|
+      hash["factor_#{element['id']}".to_sym] = factor(element['id'])
+    end
+
+    attributes_hash.merge(factors_hash)
   end
 
   private
 
+  def self.db
+    @db ||= DbAdapter.instance
+  end
+
   def trihalomethanes
     TRIHALOMETHANES
+  end
+
+  def attributes
+    ATTRIBUTES
+  end
+
+  def all_factor_weights
+    self.class.db.client.query("SELECT * FROM factor_weights")
+  end
+
+  def find_factor_weights(factor_weights_id)
+    self.class.db.client.query(
+      "SELECT * FROM factor_weights WHERE id = #{factor_weights_id}"
+    ).first
   end
 end
